911f902d8fca9ade53ce27d2cbafa5685801ac72
fjnkt98 <fjnkt98@gmail.com>
2022-03-01 21:47:22 +0900

    Change checkbox color

--- components/Content.tsx
+++ components/Content.tsx
@@ -114,7 +114,7 @@ export const Content: React.VFC<Props> = (props) => {
                 <input
                   type="checkbox"
                   checked={completed}
-                  className="inline-block mx-4 h-6 w-6 rounded-xl border-gray-300 focus:border-indigo-300 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 text-indigo-500"
+                  className="inline-block mx-4 h-6 w-6 rounded-xl border-gray-300 focus:border-teal-300 focus:ring-2 focus:ring-teal-200 focus:ring-opacity-50 text-teal-500"
                   onChange={() => {
                     dispatch({ type: 'update', index });
                   }}
