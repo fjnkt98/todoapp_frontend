ca341f0029189e9d51d0aa6515776849bb861e1d
fjnkt98 <fjnkt98@gmail.com>
2022-03-02 21:26:15 +0900

    Organize prettier options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,8 @@
-{
-  "trailingComma": "es5",
-  "semi": true,
-  "singleQuote": true
-}
+module.exports = {
+  trailingComma: 'es5',
+  semi: true,
+  singleQuote: true,
+
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.js',
+};
